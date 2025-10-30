4cd17a6a153e9b894d2b055987d97f462148aaf3
Elian <elianacuna98@gmail.com>
2025-10-30 11:12:28 -0300

    lo mismo que el anterior commit pero mejor optimisado

--- src/routes/worckspace.routes.js
+++ src/routes/worckspace.routes.js
@@ -9,16 +9,18 @@ import authMiddleware from '../middleware/auth.middleware.js'
 
 const workspace_router = express.Router()
 
+workspace_router.use( authMiddleware )
 
-workspace_router.get('/', authMiddleware, WorkspaceController.getAll )
+
+workspace_router.get('/',  WorkspaceController.getAll )
 
 
-workspace_router.get('/:workspace_id', authMiddleware, WorkspaceController.getById )
+workspace_router.get('/:workspace_id',  WorkspaceController.getById )
 
 //Crear el WorkspaceController con los metodos .post, .getById, getAll
 
 //Este es el endpoint para crear workspaces
-workspace_router.post('/', authMiddleware, WorkspaceController.post)
+workspace_router.post('/',  WorkspaceController.post)
 
 
 
